12efc898ffb9a70682cb2d4d8457e425dc8b5aa1
Lucas Galdino <127210863+galdinix@users.noreply.github.com>
2024-03-16 09:15:15 -0300

    Delete gerenciaodor_tarefas directory

--- gerenciaodor_tarefas/menu.py
+++
@@ -1,9 +0,0 @@
-from util import *
-def exibirMenu():
-    print("*** Menu Principal ***")
-    print("    [1]-Adicionar tarefa")
-    print("    [2]-Atualizar status de tarefa")
-    print("    [3]-Excluir")
-    print("    [4]-Consultar tarefas pendentes")
-    print("    [0]-Sair")
-   
